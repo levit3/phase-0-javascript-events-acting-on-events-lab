c1d6e3715d528faac9b454d09491742bf3a3d783
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-03 17:13:22 +0300

    adds DOM methods

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.left = "0px";
+
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
